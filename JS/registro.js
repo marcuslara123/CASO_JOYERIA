ae37ee33bc954455fc0493eb0cd01cd2f6aa70d9
marcuslara123 <ma.larao@duocuc.cl>
2023-04-23 17:18:47 -0400

    #

--- JS/registro.js
+++ JS/registro.js
@@ -3,11 +3,27 @@ function validarFormulario() {
 	var nombre = document.forms["registro"]["nombre"].value;
 	var correo = document.forms["registro"]["correo"].value;
 	var contrasena = document.forms["registro"]["contrasena"].value;
-	if (nombre == "" || correo == "" || contrasena == "") {
-		alert("Por favor complete todos los campos.");
+	var fecha_nac = document.forms["registro"]["fecha_nacimiento"].value;
+	
+	
+	
+	
+
+	if (nombre == "" || correo == "" || contrasena == "" || fecha_nac == "") {
+		
+		alert("POR FAVOR COMPLETAR TODOS LOS CAMPOS.");
+		
 		return false;
 		
 	}
+	if (contrasena.length < 8) {
+		alert("CONTRASEÑA DEBE SER MAYOR A 8 CARACTERES.");
+
+		return false;
+	  }
+	
+		
+
 	else  {
 		alert("REGISTRO COMPLETADO   ☑");
 	}
