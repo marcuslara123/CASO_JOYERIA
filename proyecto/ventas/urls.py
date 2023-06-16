3cd67c2aad91a04187a999558465424a2b716e37
Ignaciolr5 <ig.leyton@duocuc.cl>
2023-06-16 00:24:44 -0400

    #

--- proyecto/ventas/urls.py
+++ proyecto/ventas/urls.py
@@ -20,4 +20,9 @@ urlpatterns = [
     path('buscarCliente/<str:pk>',views.buscar_cliente,name='cliente_findEdit'),
     path('actualizarCliente',views.actualizar_cliente,name='actualizar_cliente'),
 
+    path('listar_articulos', views.mostrar_articulos, name='crud_articulos'),
+    path('agregar_articulos',views.agregar_articulos,name='articulosAdd'),
+    path('borrar_articulos/<str:pk>',views.borrar_articulo,name='articulos_del'),
+    path('actualizar_articulos/<str:pk>',views.actualizar_articulo,name='articulos_edit'),
+
      ]
