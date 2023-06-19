31a6e1f56528aa388a8469acd698d5863fa13e2b
marcuslara123 <ma.larao@duocuc.cl>
2023-06-18 20:37:29 -0400

    #

--- proyecto/ventas/models.py
+++ proyecto/ventas/models.py
@@ -5,13 +5,13 @@ from django.db import models
 
 class Articulo(models.Model):
     id_articulo         = models.AutoField(db_column='idArticulo', primary_key=True) 
-    articulo     = models.CharField(max_length=20)
+    articulo            = models.CharField(max_length=20)
     precio              = models.IntegerField()
     stock               = models.IntegerField()
     fecha_producto      = models.DateField(blank=False, null=False) 
 
     def __str__(self):
-        return str(self.articulo )+" "+str(self.nombre_articulo)   
+        return str(self.articulo )
     
 
 class Cliente(models.Model):
@@ -23,7 +23,7 @@ class Cliente(models.Model):
     telefono         = models.CharField(max_length=45)
     email            = models.EmailField(unique=True, max_length=100, blank=True, null=True)
     direccion        = models.CharField(max_length=50, blank=False, null=False) 
-    id_articulo         = models.ForeignKey('Articulo',on_delete=models.CASCADE, db_column='idArticulo') 
+    id_articulo       = models.ForeignKey('Articulo',on_delete=models.CASCADE, db_column='idArticulo') 
     activo           = models.IntegerField()
 
     def __str__(self):
